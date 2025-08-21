4d22b709888534bf8159c5c278c7cae1ddcb43d9
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-08-21 15:07:04 -0700

    tests created

--- fibonnacci.js
+++ fibonnacci.js
@@ -1,6 +1,6 @@
 function fib(times) {}
 
-console.log(fib(3));
-console.log(fib(6));
-console.log(fib(8));
-console.log(fib(15));
+// TESTS
+console.log(fib(6)); // [0, 1, 1, 2, 3, 5]
+console.log(fib(15)); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
+console.log(fib(20)); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181]
