3f3b9b6a3f90349439c552583984f209159b0510
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-08-22 20:48:17 -0700

    need final array to go up stack

--- fibonnacci.js
+++ fibonnacci.js
@@ -1,19 +1,33 @@
-function fib(times) {
+function fib(times, array = [0, 1]) {
+  console.log("processing...");
+  console.log("beginning array: ", array);
   // base case
-  if (times === 0) {
-    return;
-  }
+  if (times < 3) {
+    console.log("final array to return: ", array);
+    return array;
+  } else {
+    const secondToLastIndex = array[array.length - 2];
+    const lastIndex = array[array.length - 1];
+    const sum = secondToLastIndex + lastIndex;
 
-  let array = [];
+    console.log(
+      "secondToLastIndex: ",
+      secondToLastIndex,
+      ", lastIndex: ",
+      lastIndex,
+      ", sum: ",
+      sum
+    );
 
-  if (times > 0) {
     times = times - 1;
 
-    return;
+    array.push(sum);
+
+    return array.push(fib(times, array));
   }
 }
 
 // TESTS
 console.log(fib(6)); // [0, 1, 1, 2, 3, 5]
-console.log(fib(15)); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
-console.log(fib(20)); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181]
+// console.log(fib(15)); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
+// console.log(fib(20)); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181]
