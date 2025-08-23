2be48857a966c206bf6c6abf2c18292e4a64c628
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-08-22 20:56:03 -0700

    recursive algorithm complete, removed comments

--- fibonnacci.js
+++ fibonnacci.js
@@ -1,33 +1,21 @@
 function fib(times, array = [0, 1]) {
-  console.log("processing...");
-  console.log("beginning array: ", array);
-  // base case
+  // base case, final array that is returned up the stack
   if (times < 3) {
-    console.log("final array to return: ", array);
     return array;
   } else {
     const secondToLastIndex = array[array.length - 2];
     const lastIndex = array[array.length - 1];
     const sum = secondToLastIndex + lastIndex;
 
-    console.log(
-      "secondToLastIndex: ",
-      secondToLastIndex,
-      ", lastIndex: ",
-      lastIndex,
-      ", sum: ",
-      sum
-    );
-
     times = times - 1;
 
     array.push(sum);
 
-    return array.push(fib(times, array));
+    return fib(times, array);
   }
 }
 
 // TESTS
 console.log(fib(6)); // [0, 1, 1, 2, 3, 5]
-// console.log(fib(15)); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
-// console.log(fib(20)); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181]
+console.log(fib(15)); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
+console.log(fib(20)); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181]
