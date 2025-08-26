8ce0ec37431fe5aa452cebe0b0d259551740ed6b
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-08-25 18:43:23 -0700

    established base case

--- mergeSort.js
+++ mergeSort.js
@@ -1,13 +1,17 @@
 function mergeSort(array) {
+  // base case
+  if (array.length == 1) {
+    return array[0];
+  }
+
   let leftArray = array.slice(0, array.length / 2);
   let rightArray = array.slice(array.length / 2, array.length);
-  console.log("leftArray: ", leftArray);
-  console.log("rightArray: ", rightArray);
+
   return array;
 }
 
 // Tests
-// console.log(mergeSort([15, 9, 3, 7, 1, 5]));
+console.log(mergeSort([15, 9, 3, 7, 1, 5]));
 // console.log(mergeSort([31, 2, 5, 19, 22, 3, 11, 7]));
 // console.log(mergeSort([11, 15, 6, 7, 1, 14, 23]));
-console.log(mergeSort([32, 25, 17, 19, 55, 17, 26, 4, 1, 15]));
+// console.log(mergeSort([32, 25, 17, 19, 55, 17, 26, 4, 1, 15]));
