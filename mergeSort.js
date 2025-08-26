29fc8b0c6d1d1802c40f844cae7ca8714f259b1f
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-08-25 17:31:36 -0700

    initial commit

---
+++ mergeSort.js
@@ -0,0 +1,9 @@
+function mergeSort(array) {
+  return array;
+}
+
+// Tests
+console.log(mergeSort([15, 9, 3, 7, 1, 5]));
+console.log(mergeSort([31, 2, 5, 19, 22, 3, 11, 7]));
+console.log(mergeSort([11, 15, 6, 7, 1, 14, 23]));
+console.log(mergeSort([32, 25, 17, 19, 55, 17, 26, 4, 1, 15]));
